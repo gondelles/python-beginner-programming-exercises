2d92c002806b377066b1f744dd80df4ad401c3d7
gondelles <carlosluis.gondelles@gmail.com>
2025-09-16 09:29:30 +0000

    Ejercicios 4 Geeks finalizados - Tutorial Beginner

--- .learn/resets/17-Russian-Roulette/app.py
+++ .learn/resets/17-Russian-Roulette/app.py
@@ -8,10 +8,8 @@ def spin_chamber():
 
 # ❌ ⬆ DON'T CHANGE THE CODE ABOVE ⬆ ❌
 def fire_gun():
-	# ✅ ↓ your loop here ↓ ✅
-	if spin_chamber() == bullet_position:
-		return "You are dead!"
-	else:
-		return "Keep playing!"
+	# ✅ ↓ your code here ↓ ✅
+	return None
+
 
 print(fire_gun())
